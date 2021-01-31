ac6fedd237f68e903abfbc0e64afc4eee217f4f0
cmhoove14 <cmhoove14@gmail.com>
2021-01-30 23:39:59 -0800

    Calibration scripts

---
+++ bash/calibrate_bta01_03_sip05.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+# 
+#$ -cwd 
+#$ -V 
+#$ -j y 
+#$ -S /bin/bash 
+#$ -M choover@berkeley.edu
+#$ -m beas
+# 
+ 
+mpirun -n 1 Rscript Analysis/Calibrate/10-Calibrate-Parallel.R 0.1 0.3 1 1 0.5 3 "data/processed/data_inputs_calibrate.rds" "data/processed/input_pars_calibrate.rds" "data/processed/vax65p_scenario.rds"
