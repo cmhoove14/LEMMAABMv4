908087cdfe751f43af0b89a0cf56cb76be23dd17
cmhoove14 <cmhoove14@gmail.com>
2021-02-01 13:22:48 -0800

    Update beta given outputs from calibration

--- bash/Dec_Start_vaxessS.sh
+++ bash/Dec_Start_vaxessS.sh
@@ -8,4 +8,4 @@
 #$ -m beas
 # 
  
-Rscript Analysis/Dec_Start/12-Dec-Start-vax-runs-parallel.R 0.25 1 1 0.33 1 "data/processed/data_inputs_Dec_Start.rds" "data/processed/input_pars_Dec_start.rds" "data/processed/vaxess_scenario.rds" "data/processed/Dec_start_vaxessS/" "T" "S" "S" "F" "T"
+Rscript Analysis/Dec_Start/12-Dec-Start-vax-runs-parallel.R 0.3 1 1 0.33 1 "data/processed/data_inputs_Dec_Start.rds" "data/processed/input_pars_Dec_start.rds" "data/processed/vaxess_scenario.rds" "data/processed/Dec_start_vaxessS/" "T" "S" "S" "F" "T"
