c24591d6b96e2bafba0c4ea0f92f45e3ea8cd2fe
cmhoove14 <cmhoove14@gmail.com>
2021-02-04 16:29:51 -0800

    Testing out calibration before moving to lots of sims

---
+++ Analysis/Calibrate/10-Calibrate-Test.R
@@ -0,0 +1,32 @@
+# Test run
+
+devtools::load_all() # library(LEMMAABMv4)
+library(tictoc)
+library(progress)
+
+input_pars  <- readRDS(here::here("data/processed/input_pars_calibrate.rds"))
+data_inputs <- readRDS(here::here("data/processed/data_inputs_calibrate.rds"))
+vax_phases  <- readRDS(here::here("data/processed/vax65p_scenario.rds"))
+
+visitors <- TRUE 
+testing <- "S" 
+vaccination <- FALSE 
+verbose <- TRUE 
+store_extra <- FALSE 
+
+set.seed(430)
+
+tic()
+
+time_run <- LEMMAABMv4::covid_abm_v4(data_inputs = data_inputs, 
+                                     input_pars  = input_pars, 
+                                     vax_phases  = vax_phases,
+                                     visitors    = visitors, 
+                                     testing     = testing, 
+                                     vaccination = vaccination,
+                                     verbose     = verbose, 
+                                     store_extra = store_extra)
+
+saveRDS(time_run, here::here("data", "outputs", "Calibrate_test_Run.rds"))
+
+toc()
