d20893cae457323cdd02d7d3a246e52689765322
cmhoove14 <cmhoove14@gmail.com>
2021-02-08 13:11:12 -0800

    Prep for wynton submission

--- bash/LHS_Calibration_Wynton/LHS1_360_Calibrate.sh
+++ bash/LHS_Calibration_Wynton/LHS1_360_Calibrate.sh
@@ -1,14 +1,13 @@
 #!/bin/bash                        #-- what is the language of this shell
 #                                  #-- Any line that starts with #$ is an instruction to SGE
 #$ -S /bin/bash                    #-- the shell for the job
-#$ -o [dir]                        #-- output directory (fill in)
-#$ -e [dir]                        #-- error directory (fill in)
 #$ -cwd                            #-- tell the job that it should start in your working directory
 #$ -j y                            #-- tell the system that the STDERR and STDOUT should be joined
 #$ -l mem_free=5G                  #-- submits on nodes with enough free memory (required)
 #$ -l scratch=80G                  #-- SGE resources (home and scratch disks)
-#$ -l h_rt=48:00:00                #-- runtime limit (see above; this requests 24 hours)
+#$ -l h_rt=04:00:00                #-- runtime limit (see above; this requests 24 hours)
 ##$ -t 1-10                        #-- remove first '#' to specify the number of
                                    #-- tasks if desired (see Tips section on this page)
-
-Rscript Analysis/Calibrate/12-Calibrate-Parallel.R 1 64 "data/processed/data_inputs_calibrate.rds" "data/processed/input_pars_calibrate.rds" "data/processed/vax65p_scenario.rds" "data/outputs/Calibration_Outputs/" "T" "S" "F" "F" "F"
+module load CBI r
+module load scl-devtoolset/8
+Rscript Analysis/Calibrate/12-Calibrate-Parallel-Wynton.R 1 360 "data/processed/data_inputs_calibrate.rds" "data/processed/input_pars_calibrate.rds" "data/processed/vax65p_scenario.rds" "data/outputs/Calibration_Outputs/" "T" "S" "F" "F" "F"
