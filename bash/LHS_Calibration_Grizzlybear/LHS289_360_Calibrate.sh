8f09b474143e7c03b938b543cbb3433a28909763
cmhoove14 <cmhoove14@gmail.com>
2021-02-07 21:34:04 -0800

    tRY INDICATING NUMBER OF THREADS FOR SPEED INCREASE

--- bash/LHS_Calibration_Grizzlybear/LHS289_360_Calibrate.sh
+++ bash/LHS_Calibration_Grizzlybear/LHS289_360_Calibrate.sh
@@ -8,4 +8,5 @@
 #$ -M choover@berkeley.edu
 # 
 
+export OMP_NUM_THREADS=1 
 Rscript Analysis/Calibrate/12-Calibrate-Parallel.R 289 360 "data/processed/data_inputs_calibrate.rds" "data/processed/input_pars_calibrate.rds" "data/processed/vax65p_scenario.rds" "data/outputs/LHS_Calibration/" "T" "S" "F" "F" "F"
