7c94d78fbb5713809a0b37aef07429a4d000d8cd
cmhoove14 <cmhoove14@gmail.com>
2021-02-09 17:08:53 -0800

    Add logs2 directory

--- bash/LHS_Calibration_Wynton/LHS_Array_Calibrate2.sh
+++ bash/LHS_Calibration_Wynton/LHS_Array_Calibrate2.sh
@@ -2,8 +2,8 @@
 #                                  #-- Any line that starts with #$ is an instruction to SGE
 #$ -S /bin/bash                    #-- the shell for the job
 #$ -cwd                            #-- tell the job that it should start in your working directory
-#$ -o data/outputs/Logs/job-$TASK_ID.stdout #-- out directory
-#$ -e data/outputs/Logs/job-$TASK_ID.stderr #-- error directory
+#$ -o data/outputs/Logs2/job-$TASK_ID.stdout #-- out directory
+#$ -e data/outputs/Logs2/job-$TASK_ID.stderr #-- error directory
 #$ -j y                            #-- tell the system that the STDERR and STDOUT should be joined
 #$ -l mem_free=5G                  #-- submits on nodes with enough free memory (required)
 #$ -l scratch=70G                  #-- SGE resources (home and scratch disks)
