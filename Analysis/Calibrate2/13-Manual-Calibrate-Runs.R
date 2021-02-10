04e6bda97475b15967c5b4cb6a9c5da813c074aa
cmhoove14 <cmhoove14@gmail.com>
2021-02-10 12:15:23 -0800

    ignore data in build, manual configuration of some calibration runs, and add hospitalizations by wave to calibration plots

---
+++ Analysis/Calibrate2/13-Manual-Calibrate-Runs.R
@@ -0,0 +1,39 @@
+# ---------------------------------------
+# Manual calibration runs
+# Chris Hoover Feb 2021
+# ---------------------------------------
+
+# Load data files from bash paths ---------------------
+input_pars  <- readRDS(here::here("data","processed","input_pars_calibrate.rds"))
+data_inputs <- readRDS(here::here("data", "processed", "data_inputs_calibrate.rds"))
+vax_phases  <- readRDS(here::here("data", "processed", "vax65p_scenario.rds"))
+output_path <- here::here("Scratch")
+
+  visitors    <- TRUE 
+  testing     <- "S" 
+  vaccination <- FALSE 
+  verbose     <- FALSE 
+  store_extra <- TRUE 
+  
+# Replace pars in list with manual pars -------------------
+
+input_pars$trans_pars$bta_base             <- 0.26
+input_pars$trans_pars$bta_hh               <- 1.4
+input_pars$trans_pars$bta_work             <- 1.1
+input_pars$trans_pars$bta_sip_rd           <- 0.33
+input_pars$trans_pars$hpi_bta_mult         <- 0.25
+
+input_pars$other_pars$mort_mult            <- 0.5
+input_pars$init_states$E0                  <- 3
+
+devtools::load_all()
+
+covid_abm_v4(data_inputs = data_inputs, 
+             input_pars  = input_pars, 
+             vax_phases  = vax_phases,
+             visitors    = visitors, 
+             testing     = testing, 
+             vaccination = vaccination,
+             verbose     = verbose, 
+             store_extra = store_extra,
+             output_path = paste0(output_path,"/"))
