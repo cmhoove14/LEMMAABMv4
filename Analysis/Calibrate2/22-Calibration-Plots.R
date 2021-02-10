4c131187de053ce978072ea11214d4dacbfe7d08
cmhoove14 <cmhoove14@gmail.com>
2021-02-10 10:32:12 -0800

    Update best plots

--- Analysis/Calibrate2/22-Calibration-Plots.R
+++ Analysis/Calibrate2/22-Calibration-Plots.R
@@ -5,22 +5,26 @@
 library(tidyverse)
 library(data.table)
 
-fits <- readRDS(here::here("data", "processed", "LHS_Fits1_summary.rds"))
+fits <- readRDS(here::here("data", "processed", "LHS_Fits2_summary.rds"))
 
 # Sims with best fits to individual categories and best overall
-best_hosp      <- fits$sim[which.min(fits$hosp_mse)]
-best_dths      <- fits$sim[which.min(fits$dths_mse)]
-best_dths_race <- fits$sim[which.min(fits$dths_race_mse)]
-best_ct_cases  <- fits$sim[which.min(fits$ct_cases_mse)]
-best_case_race <- fits$sim[which.min(fits$case_race_mse)]
-best_overall   <- fits$sim[which.min(fits$overall_z)]
-best10         <- fits$sim[which(fits$overall_z <= quantile(fits$overall_z, 10/nrow(fits)))]
+best_hosp           <- fits$sim[which.min(fits$hosp_fit)]
+best_dths           <- fits$sim[which.min(fits$dths_fit)]
+best_dths_race      <- fits$sim[which.min(fits$dths_race_fit)]
+best_ct_cases       <- fits$sim[which.min(fits$ct_cases_fit)]
+best_case_race      <- fits$sim[which.min(fits$case_race_fit)]
+best_overall        <- fits$sim[which.min(fits$overall_fit)]
+best_overall_norm   <- fits$sim[which.min(fits$overall_fit_norm)]
+best10              <- fits$sim[which(fits$overall_fit <= quantile(fits$overall_fit, 10/nrow(fits)))]
+best10_no_ct        <- fits$sim[which(fits$hosp_dths_race_fit <= quantile(fits$hosp_dths_race_fit, 10/nrow(fits)))]
+best10_norm         <- fits$sim[which(fits$overall_fit_norm <= quantile(fits$overall_fit_norm, 10/nrow(fits)))]
+best10_no_ct_norm   <- fits$sim[which(fits$hosp_dths_race_fit_norm <= quantile(fits$hosp_dths_race_fit_norm, 10/nrow(fits)))]
 
 # Get CA & SF data
 load(here::here("data", "get", "got", "CA_SF_data2021-02-04.Rdata"))
 
 # Utils to grab simulation/fit files
-sims_root <- here::here("data","outputs","Calibration_Outputs")
+sims_root <- here::here("data","outputs","Calibration2_Outputs")
 
 get_sim <- function(sim){
   # Get sim file  
@@ -30,7 +34,7 @@ get_sim <- function(sim){
   return(sim)
 }
 
-fits_root <- here::here("data","outputs","Calibration_Fits")
+fits_root <- here::here("data","outputs","Calibration2_Fits")
 
 get_fit <- function(sim){
   # Get sim file  
@@ -41,7 +45,7 @@ get_fit <- function(sim){
 }
 
 # Get input pars for reference dates
-best_overall_sim <- get_sim(best_overall)
+best_overall_sim <- get_sim(best_overall_norm)
 input_pars  <- best_overall_sim$input_pars
 LEMMAABMv4::unpack_list(input_pars)
 
@@ -52,7 +56,7 @@ par(mar = c(3,2,2,0.1),
 # Compare daily hospitalizations ----------------
 best_hosp_sim <- get_fit(best_hosp)$hosp
 
-jpeg(here::here("Plots", "LHS_Calibration", "Fits1", "hosp_sims_best.jpg"),
+jpeg(here::here("Plots", "LHS_Calibration", "Fits2", "hosp_sims_best.jpg"),
      height = 4, width = 7, units = "in", res = 100)
 
 plot(x    = sf_hosp$Date[which(sf_hosp$Date <= t.end)], 
@@ -64,14 +68,14 @@ plot(x    = sf_hosp$Date[which(sf_hosp$Date <= t.end)],
 
   lines(best_hosp_sim$Date, best_hosp_sim$N, col = "red")
 
-  for(s in best10){
+  for(s in best10_norm){
     hosp_sim <- get_fit(s)$hosp
     lines(hosp_sim$Date, hosp_sim$N, col = "blue")
   }
   
   legend("topleft", bty = "n",
          lty = 1, col = c("red", "blue"),
-         legend = c("Best fit", "Top10 Overall"),
+         legend = c("Best hosp fit", "Top10 Overall"),
          cex = 0.7)
   
 dev.off()  
@@ -85,7 +89,7 @@ best_dths_sim <- as_tibble(get_sim(best_dths)$agents[state == "D", c("id", "sex"
   group_by(tod) %>% 
   summarise(n_d_sim = n())
 
-jpeg(here::here("Plots", "LHS_Calibration", "Fits1", "dths_sims_best.jpg"),
+jpeg(here::here("Plots", "LHS_Calibration", "Fits2", "dths_sims_best.jpg"),
      height = 4, width = 7, units = "in", res = 100)
 
 plot(x    = sf_case$Date[which(sf_case$Date <= t.end)], 
@@ -97,7 +101,7 @@ plot(x    = sf_case$Date[which(sf_case$Date <= t.end)],
 
 lines(best_dths_sim$tod, best_dths_sim$n_d_sim, col = "red")
 
-for(s in best10){
+for(s in best10_norm){
   dths_sim <- as_tibble(get_sim(s)$agents[state == "D", c("id", "sex", "age", "race", "t_death")]) %>% 
     mutate(
       tod = as.Date(as.character(zoo::as.Date.numeric(t_death)))
@@ -140,7 +144,7 @@ best_dths_race_sim %>%
        fill = "",
        title = "Cumulative Deaths by Race fit")
 
-ggsave(here::here("Plots", "LHS_Calibration", "Fits1", "dths_race_sims_best.jpg"),
+ggsave(here::here("Plots", "LHS_Calibration", "Fits2", "dths_race_sims_best.jpg"),
        units="in", width = 5, height = 3)
 
 # Compare cumulative Dec 1 confirmed cases by race ----------------------
@@ -170,7 +174,7 @@ best_case_race_sim %>%
        fill = "",
        title = "Cumulative Cases by Race fit")
 
-ggsave(here::here("Plots", "LHS_Calibration", "Fits1", "case_race_sims_best.jpg"),
+ggsave(here::here("Plots", "LHS_Calibration", "Fits2", "case_race_sims_best.jpg"),
        units="in", width = 7, height = 5)
 
 # Compare CT Cases by month ----------------------
@@ -185,6 +189,6 @@ best_ct_cases_sim %>%
     theme_classic() +
     theme(legend.position = "none")
     
-ggsave(here::here("Plots", "LHS_Calibration", "Fits1", "ct_cases_sims_best.jpg"),
+ggsave(here::here("Plots", "LHS_Calibration", "Fits2", "ct_cases_sims_best.jpg"),
        units="in", width = 7, height = 5)
 
