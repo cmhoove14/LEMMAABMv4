00d54cbb60166410c4d321f423e69a5eec57ef94
cmhoove14 <cmhoove14@gmail.com>
2021-02-12 11:02:48 -0800

    Update fit metrics

--- Analysis/Calibrate/20-2-Calibration-Fit-Scores-NLL-Wynton.R
+++ Analysis/Calibrate/20-2-Calibration-Fit-Scores-NLL-Wynton.R
@@ -132,13 +132,18 @@ gc()
 
 out_list <- list()
 
-out_list$fits <- list("sim"           = taskID, 
-                      "status"        = 1,
-                      "hosp_mse"      = hosp_nll,
-                      "dths_mse"      = dths_nll,
-                      "dths_race_mse" = dths_race_nll,
-                      "ct_cases_mse"  = ct_cases_nll,
-                      "case_race_mse" = case_race_nll)
+out_list$fits <- list("sim"                = taskID, 
+                      "status"             = 1,
+                      "hosp_fit"           = hosp_nll,
+                      "dths_fit"           = dths_nll,
+                      "dths_race_fit"      = dths_race_nll,
+                      "ct_cases_fit"       = ct_cases_nll,
+                      "case_race_fit"      = case_race_nll,
+                      "hosp_fit_norm"      = hosp_nll/nrow(comp_hosp),
+                      "dths_fit_norm"      = dths_nll/nrow(comp_dths),
+                      "dths_race_fit_norm" = dths_race_nll/nrow(comp_dths_race),
+                      "ct_cases_fit_norm"  = ct_cases_nll/nrow(comp_ct_cases),
+                      "case_race_fit_norm" = case_race_nll/nrow(comp_case_race))
 
 out_list$comp_dfs <- list("hosp"      = comp_hosp,
                           "dths"      = comp_dths,
--- Analysis/Calibrate/21-Process-Calibrations-Wynton.R
+++ Analysis/Calibrate/21-Process-Calibrations-Wynton.R
@@ -9,7 +9,7 @@ library(zoo)
 # Folders containing lists of outputs on wynton
 root <- here::here("data","outputs","Calibration_Fits")
 
-n_tasks <- 1440
+n_tasks <- 2000
 
 fitsdf <- bind_rows(lapply(1:n_tasks, function(x){
   # Get sim file  
@@ -35,12 +35,16 @@ get_z <- function(vec){
 
 fitsdf_out <- fitsdf %>% 
   mutate(
-    hosp_z      = get_z(hosp_mse),
-    dths_z      = get_z(dths_mse),
-    dths_race_z = get_z(dths_race_mse),
-    ct_cases_z  = get_z(ct_cases_mse),
-    case_race_z = get_z(case_race_mse),
-    overall_z   = hosp_z + dths_z + dths_race_z + ct_cases_z + case_race_z
+    hosp_z                  = get_z(hosp_fit),
+    dths_z                  = get_z(dths_fit),
+    dths_race_z             = get_z(dths_race_fit),
+    ct_cases_z              = get_z(ct_cases_fit),
+    case_race_z             = get_z(case_race_fit),
+    overall_z               = hosp_z + dths_z + dths_race_z + ct_cases_z + case_race_z,
+    overall_fit             = hosp_fit+dths_fit+dths_race_fit+ct_cases_fit+case_race_fit,
+    hosp_dths_race_fit      = hosp_fit+dths_fit+dths_race_fit+case_race_fit,
+    overall_fit_norm        = hosp_fit_norm+dths_fit_norm+dths_race_fit_norm+ct_cases_fit_norm+case_race_fit_norm,
+    hosp_dths_race_fit_norm = hosp_fit_norm+dths_fit_norm+dths_race_fit_norm+case_race_fit_norm
   ) %>% 
   arrange(-overall_z)
 
