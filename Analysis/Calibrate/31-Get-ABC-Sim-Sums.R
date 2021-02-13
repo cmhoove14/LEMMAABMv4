7eda410ca6b880142516832be9b6e94271233da1
cmhoove14 <cmhoove14@gmail.com>
2021-02-12 18:11:01 -0800

    Get summary stats for all sims as array job

--- Analysis/Calibrate/31-Get-ABC-Sim-Sums.R
+++ Analysis/Calibrate/31-Get-ABC-Sim-Sums.R
@@ -3,12 +3,35 @@
 # Chris Hoover Feb 2021
 # ---------------------------------------
 library(tidyverse)
-library(sensitivity)
+library(data.table)
+library(abc)
 
-fits <- readRDS(here::here("data", "processed", "LHS_Fits1_summary.rds"))
-LHS  <- as_tibble(readRDS(here::here("data", "processed", "Calibration_LHS_Wynton.rds"))) %>% 
-  mutate(sim = row_number())
+opts <- commandArgs(TRUE)
 
-lhs_fits <- fits %>% left_join(LHS, by = "sim") %>% 
-  filter(hosp_fit > 0)
+taskID <- as.numeric(opts[1])
+
+root <- here::here("data","outputs","Calibration_Outputs")
+
+# Get sim file  
+sim_folder <- here::here(root, taskID)
+sim <- readRDS(paste0(sim_folder,"/",list.files(sim_folder)[1]))
+
+# Process sim to get sim smmaries
+sim_dths <- as_tibble(sim$agents[state == "D", c("id", "sex", "age", "race", "t_death")]) %>% 
+  mutate(
+    tod = zoo::as.Date.numeric(t_death)
+  ) %>% 
+  arrange(tod) %>%
+  right_join(dths_dates, by = c("tod" = "Date")) %>% 
+  mutate(wod =paste0(lubridate::epiweek(tod), "_",
+                     lubridate::year(tod))) %>% 
+  group_by(wod) %>% 
+  summarise(n_d_sim = sum(!is.na(id))) %>% 
+  pull(n_d_sim)
+
+
+
+sim_hosp <- as_tibble(sim$epi_curve[state == "Ih",])
+sim_hosp$Date <- as.Date(as.character(sim_hosp$date)) # Date formate from sim messed up because of sub-daily time step
+
 
