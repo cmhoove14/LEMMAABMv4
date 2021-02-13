894a39ff2c464cf39d450ea497a40d30db403f59
cmhoove14 <cmhoove14@gmail.com>
2021-02-12 18:10:43 -0800

    Run ABC separate so can submit getting summary stats as array job

---
+++ Analysis/Calibrate/32-RunABC.R
@@ -0,0 +1,53 @@
+# ---------------------------------------
+# Sensitivity of model fits to different parameters
+# Chris Hoover Feb 2021
+# ---------------------------------------
+library(tidyverse)
+library(data.table)
+library(abc)
+
+opts <- commandArgs(TRUE)
+
+taskID <- as.numeric(opts[1])
+
+root <- here::here("data","outputs","Calibration_Outputs")
+
+# Get sim file  
+sim_folder <- here::here(root, taskID)
+sim <- readRDS(paste0(sim_folder,"/",list.files(sim_folder)[1]))
+
+# Get obs summaries
+load(here::here("data/processed/obs_summaries_for_ABC.Rdata"))
+
+# Process sim to get sim smmaries
+sim_dths <- as_tibble(sim$agents[state == "D", c("id", "sex", "age", "race", "t_death")]) %>% 
+  mutate(
+    tod = zoo::as.Date.numeric(t_death)
+  ) %>% 
+  arrange(tod) %>%
+  right_join(dths_dates, by = c("tod" = "Date")) %>% 
+  mutate(wod =paste0(lubridate::epiweek(tod), "_",
+                     lubridate::year(tod))) %>% 
+  group_by(wod) %>% 
+  summarise(n_d_sim = sum(!is.na(id))) %>% 
+  pull(n_d_sim)
+
+
+
+
+
+
+
+sim_hosp <- as_tibble(sim$epi_curve[state == "Ih",])
+sim_hosp$Date <- as.Date(as.character(sim_hosp$date)) # Date formate from sim messed up because of sub-daily time step
+
+comp_hosp <- merge(sim_hosp, sf_hosp, by = "Date")
+
+
+# ABC do your thang -----------------
+abc_est <- abc(target = obs_sums,
+               param = lhs_fits[,which(colnames(lhs_fits)=="bta_base"):which(colnames(lhs_fits) == "E0")],  
+               sumstat = sim_sums,  
+               tol = round(nrow(lhs_fits*0.01)),
+               transf = "log",
+               method = "ridge")
