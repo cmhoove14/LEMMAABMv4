f7803f63ea6f3b5cb6f374f77d22c3ea66ac3efc
cmhoove14 <cmhoove14@gmail.com>
2021-02-17 09:15:48 -0800

    Validation runs scripts

---
+++ Analysis/Validate/10-Validation-Runs.R
@@ -0,0 +1,62 @@
+# ---------------------
+# Validation Runs for LEMMABMv4
+# Chris Hoover Feb 2021
+# ---------------------
+
+data.table::setDTthreads(1)
+
+# Get options passed from BASH -----------------
+opts <- commandArgs(TRUE)
+
+taskID <- as.numeric(opts[1])
+data_inputs_path <- as.character(opts[2])
+input_pars_path  <- as.character(opts[3])
+vax_phases_path  <- as.character(opts[4])
+output_path      <- as.character(opts[5]) 
+
+# Get inputs ------------------
+input_pars  <- readRDS(here::here(input_pars_path))
+data_inputs <- readRDS(here::here(data_inputs_path))
+vax_phases  <- readRDS(here::here(vax_phases_path))
+
+if(length(opts) > 5){
+  visitors    <- as.logical(opts[6]) 
+  testing     <- opts[7]  
+  vaccination <- as.logical(opts[8])
+  verbose     <- as.logical(opts[9])  
+  store_extra <- as.logical(opts[10])  
+  initial     <- as.logical(opts[11])  
+} else {
+  visitors    <- TRUE 
+  testing     <- "S" 
+  vaccination <- FALSE 
+  verbose     <- FALSE 
+  store_extra <- TRUE 
+  initial     <- FALSE
+}
+
+# Randomly sample a simulation from replicates to use in this simulation ---------------
+n_rep_runs <- length(list.files(here::here("data/outputs/Best_Replicates")))
+
+use_rep    <- sample(1:n_rep_runs, 1)
+use_file   <- here::here("data/outputs/Best_Replicates", use_rep)
+
+got_rep    <- readRDS(paste0(use_file, "/", list.files(use_file)[1]))
+
+# Get agents and pars used in simulation run
+data_inputs$agents <- got_rep$agents
+input_pars         <- got_rep$input_pars
+
+# Run simulation --------------
+library(LEMMAABMv4)
+
+LEMMAABMv4::covid_abm_v4(data_inputs = data_inputs, 
+                         input_pars  = input_pars, 
+                         vax_phases  = vax_phases,
+                         visitors    = visitors, 
+                         testing     = testing, 
+                         vaccination = vaccination,
+                         verbose     = verbose, 
+                         store_extra = store_extra,
+                         initial     = initial,
+                         output_path = paste0(output_path,taskID,"/"))
---
+++ Analysis/Validate/b10-Validation-Runs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash                        #-- what is the language of this shell
+#                                  #-- Any line that starts with #$ is an instruction to SGE
+#$ -S /bin/bash                    #-- the shell for the job
+#$ -cwd                            #-- tell the job that it should start in your working directory
+#$ -o data/outputs/Logs/VALjob-$TASK_ID.stdout #-- out directory
+#$ -e data/outputs/Logs/VALjob-$TASK_ID.stderr #-- error directory
+#$ -j y                            #-- tell the system that the STDERR and STDOUT should be joined
+#$ -l mem_free=5G                  #-- submits on nodes with enough free memory (required)
+#$ -l scratch=10G                  #-- SGE resources (home and scratch disks)
+#$ -l h_rt=04:00:00                #-- runtime limit 
+#$ -t 1-200                        #-- array job
+                                   #-- tasks if desired (see Tips section on this page)
+module load CBI r
+Rscript Analysis/Validate/10-Validation-Runs.R $SGE_TASK_ID "data/processed/data_inputs_validate.rds" "data/processed/input_pars_validate.rds" "data/processed/vax65p_scenario.rds" "data/outputs/Validation_Runs/"
