6c9865d07aa01c67cfe7f7dfac055a9c16297f85
cmhoove14 <cmhoove14@gmail.com>
2021-02-19 11:13:15 -0800

    Next round

--- Analysis/Calibrate/b40-Replicate-Best-100.sh
+++ Analysis/Calibrate/b40-Replicate-Best-100.sh
@@ -2,8 +2,8 @@
 #                                  #-- Any line that starts with #$ is an instruction to SGE
 #$ -S /bin/bash                    #-- the shell for the job
 #$ -cwd                            #-- tell the job that it should start in your working directory
-#$ -o data/outputs/Logs/BestREP6job-$TASK_ID.stdout #-- out directory
-#$ -e data/outputs/Logs/BestREP6job-$TASK_ID.stderr #-- error directory
+#$ -o data/outputs/Logs/BestREP7job-$TASK_ID.stdout #-- out directory
+#$ -e data/outputs/Logs/BestREP7job-$TASK_ID.stderr #-- error directory
 #$ -j y                            #-- tell the system that the STDERR and STDOUT should be joined
 #$ -l mem_free=5G                  #-- submits on nodes with enough free memory (required)
 #$ -l scratch=10G                  #-- SGE resources (home and scratch disks)
@@ -11,4 +11,4 @@
 #$ -t 1-100                        #-- array job
                                    #-- tasks if desired (see Tips section on this page)
 module load CBI r
-Rscript Analysis/Calibrate/40-Best-Replicates.R $SGE_TASK_ID "data/outputs/Best_Replicates6/"
+Rscript Analysis/Calibrate/40-Best-Replicates.R $SGE_TASK_ID "data/outputs/Best_Replicates7/"
